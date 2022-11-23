46eaeaefb8f2948a5d12d8aa72297292ea842f99
Omar Manzo <omarenocm@gmail.com>
2022-11-23 12:29:35 -0600

    Fixed issue

--- app/src/main/java/com/omarenoc/pokedexmetaapp/ui/MainActivity.kt
+++ app/src/main/java/com/omarenoc/pokedexmetaapp/ui/MainActivity.kt
@@ -29,4 +29,9 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commitAllowingStateLoss()
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        this.finish()
+    }
 }
